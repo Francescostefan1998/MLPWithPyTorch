648205513d3625a6d27c212a9da1455eea0dbc60
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-13 11:17:32 +0200

    binary cross-entropy and categrorical cross-entropy

--- cnnwithL2.py
+++ cnnwithL2.py
@@ -9,3 +9,20 @@ loss_with_penalty = loss + l2_penalty
 linear_layer = nn.Linear(10, 16)
 l2_penalty = l2_lambda * sum([(p**2).sum() for p in linear_layer.parameters()])
 loss_with_penalty = loss + l2_penalty
+
+########## Binary Cross-entropy - used for binary classification
+logits = torch.tensor([0.8])
+probas = torch.sigmoid(logits) # applying the sigmoid to the logits
+target = torch.tensor([1.0])
+bce_loss_fn = nn.BCELoss()
+bce_logits_loss_fn = nn.BCEWithLogitsLoss()
+print(f'BCE (w Probas): {bce_loss_fn(probas, target):.4f}')
+print(f'BCE (w Logits): {bce_logits_loss_fn(logits, target):.4f}')
+# Categorical Cross-entorypy - used for multiclass classification
+logits = torch.tensor([[1.5, 0.8, 2.1]])
+probas = torch.softmax(logits, dim=1) # applying the softmax to the logits
+target = torch.tensor([2])
+cce_loss_fn = nn.NLLLoss()
+cce_logits_loss_fn = nn.CrossEntropyLoss()
+print(f'CCE (w Logits): {cce_logits_loss_fn(logits, target):.4f}')
+print(f'CCE (w Probas): {cce_loss_fn(torch.log(probas), target):.4f}')
